219fd8f340c874d697d2b3c256c88de9f1937642
juanfope <157375092+juanfope@users.noreply.github.com>
2024-05-09 16:11:49 -0500

    backend+frontendFix

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import 'boxicons'
@@ -10,6 +10,28 @@ import Post from './components/Post';
 import MyPosts from './components/MyPosts';
 
 function App() {
+
+  /*const [backendData, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, [])*/
+
+  /*<div>
+      {(typeof backendData.users === 'undefined') ? (
+        <p>loading...</p>
+      ): (
+        backendData.users.map((user, i) => (
+          <p key={i}>{user}</p>
+        ))
+      )}
+    </div>*/
 
   return (
     <>
@@ -24,7 +46,7 @@ function App() {
       </Routes>
     </Router>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
