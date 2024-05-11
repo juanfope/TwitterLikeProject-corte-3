7d4fde46d813cd8089ddcac6b0b15566eda82b11
juanfope <157375092+juanfope@users.noreply.github.com>
2024-05-10 22:11:14 -0500

    jwt-test-IGNORAR-X2

--- server/server.js
+++ server/server.js
@@ -1,58 +1,71 @@
 const express = require('express')
 const app = express()
 const jwt = require('jsonwebtoken')
-require('dotenv').config()
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
+const users=[
+    {"email": "rataratosa@gmail.com","username":"thefatrat","id":"111","password":"ratablanca"},
+    {"email": "chiguirin@gmail.com","username":"capyguiro","id":"222","password":"elviajedechiguiro"},
+    {"email": "joemama@gmail.com","username":"biden007","id":"333","password":"fucktrump"}
+]
 
-app.get("/api", (req, res) => {
-    res.json({"users": ["userOne", "userTwo", "userThree"]})
-})
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
 
-app.get("/login", (req, res) => {
-    res.send(`<html>
-                <head>
-                    <title>Login</title>
-                </head>
-                <body>
-                    <form method="POST" action="/auth">
-                        Usuario: <input type="text" name="text" /><br />
-                        Contraseña: <input type="password" name="password" /><br />
-                        <input type="submit" value="Iniciar sesion" />
-                    </form>
-                </body>`
-    )
-})
-
-app.post("/auth", (req, res) => {
-    const {username, password} = req.body
-
-    const user = {username: username}
-
-    const accessToken =generateAccessToken(user)
-
-    res.header('authorization', accessToken).json({
-        message: 'Usuario autenticado',
-        token: accessToken
+app.get("/", (req, res) => {
+    res.json({
+        message: "Hello there"
     })
 })
 
-function generateAccessToken(user){
-    return jwt.sign(user, process.env.SECRET, {expiresIn: '5m'})
-}
+app.post("/login", (req, res) => {
+    console.log('req data', req.body.password,req.body.email)
+    users.filter(user => {
+        if(user.email===req.body.email){
+            if(user.password===req.body.password){
+                console.log(user)
 
-function validateToken(req, res, next){
-    const accessToken = req.headers['authorization']
-    if(!accessToken) res.send('Acces denied')
+                const payload = {
+                    "id": user.id
+                }
 
-    jwt.verify(accessToken, process.env.SECRET, (err, user) => {
-        if(err){
-            res.send('Access denied, token expires or incorrect')
-        }else{
-            next()
+                jwt.sign(payload, 'shhh', {expiresIn: '10h'},(err,token) => {
+                    res.json({
+                        token: token,
+                    } 
+                    )
+                })
+            }
         }
     })
+})
+
+app.post("/dailymeme", verifyToken, (req,res)=>{
+    jwt.verify(req.token, 'shhh', (err, authData)=>{
+        if(err){
+            res.sendStatus(403)
+        }
+        else{
+            res.json({
+                message: "Meme charged ;D",
+                authData: authData
+            })
+        }
+    })
+})
+
+function verifyToken(req, res, next){
+    const bearerHeader = req.headers['authorization'];
+    if(typeof bearerHeader!=='undefined'){
+        const bearer = bearerHeader.split(' ');
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next()
+    }
+    else{
+        res.sendStatus(403)
+    }
 }
 
 app.listen(5000, () => {console.log("Server started on port 5000") })
